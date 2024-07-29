6f3feaa6fded7c9b563687fec159b8f3baffb2aa
Deven Shah <devenshah1974@gmail.com>
2024-07-29 09:18:33 +0100

    CSP-1567 Implements Recruitment filters

--- src/SFA.DAS.Provider.PR.Web/Models/HomeViewModel.cs
+++ src/SFA.DAS.Provider.PR.Web/Models/HomeViewModel.cs
@@ -22,9 +22,12 @@ public class HomeSubmitModel
     public const string HasCreateCohortPermissionKey = "HasCreateCohortPermission";
 
     public string? SearchTerm { get; set; }
-    public bool PendingRequest { get; set; }
+    public bool HasPendingRequest { get; set; }
     public bool HasAddApprenticePermission { get; set; }
-    public bool HasNotAddApprenticePermission { get; set; }
+    public bool HasNoAddApprenticePermission { get; set; }
+    public bool HasRecruitmentPermission { get; set; }
+    public bool HasRecruitmentWithReviewPermission { get; set; }
+    public bool HasNoRecruitmentPermission { get; set; }
 
     public Dictionary<string, string> ToQueryString()
     {
@@ -32,9 +35,16 @@ public class HomeSubmitModel
 
         if (!string.IsNullOrWhiteSpace(SearchTerm)) result.Add(nameof(SearchTerm), SearchTerm.Trim());
 
-        if (PendingRequest) result.Add(nameof(PendingRequest), true.ToString());
+        if (HasPendingRequest) result.Add(nameof(HasPendingRequest), true.ToString());
 
-        if (HasNotAddApprenticePermission != HasAddApprenticePermission) result.Add(HasCreateCohortPermissionKey, HasAddApprenticePermission.ToString());
+        if (HasNoAddApprenticePermission != HasAddApprenticePermission) result.Add(HasCreateCohortPermissionKey, HasAddApprenticePermission.ToString());
+
+        if (HasRecruitmentPermission || HasRecruitmentWithReviewPermission || HasNoRecruitmentPermission)
+        {
+            result.Add(nameof(HasRecruitmentPermission), HasRecruitmentPermission.ToString());
+            result.Add(nameof(HasRecruitmentWithReviewPermission), HasRecruitmentWithReviewPermission.ToString());
+            result.Add(nameof(HasNoRecruitmentPermission), HasNoRecruitmentPermission.ToString());
+        }
 
         return result;
     }
