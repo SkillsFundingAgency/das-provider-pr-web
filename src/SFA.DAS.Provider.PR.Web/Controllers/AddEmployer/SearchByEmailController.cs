7d500407d41599e1d187d2b38ea6478ce02f5e32
MarkFCain <mark.cain@digital.education.gov.uk>
2024-08-06 10:06:57 +0100

    CSP-1351 Get Account with no relationship

--- src/SFA.DAS.Provider.PR.Web/Controllers/AddEmployer/SearchByEmailController.cs
+++ src/SFA.DAS.Provider.PR.Web/Controllers/AddEmployer/SearchByEmailController.cs
@@ -20,6 +20,7 @@ public class SearchByEmailController(IOuterApiClient _outerApiClient, ISessionSe
 {
     public const string ViewPath = "~/Views/AddEmployer/SearchByEmail.cshtml";
     public const string MultipleAccountsShutterPathViewPath = "~/Views/AddEmployer/MultipleAccountsShutterPage.cshtml";
+    public const string OneAccountNoRelationshipViewPath = "~/Views/AddEmployer/OneAccountNoRelationship.cshtml";
 
     [HttpGet]
     public IActionResult Index([FromRoute] int ukprn)
@@ -30,6 +31,7 @@ public class SearchByEmailController(IOuterApiClient _outerApiClient, ISessionSe
         {
             viewModel.Email = sessionModel.Email;
         }
+
         return View(ViewPath, viewModel);
     }
 
@@ -65,6 +67,11 @@ public class SearchByEmailController(IOuterApiClient _outerApiClient, ISessionSe
             return RedirectToRoute(RouteNames.AddEmployerMultipleAccounts, new { ukprn });
         }
 
+        if (relationshipByEmail.HasRelationship != null && !relationshipByEmail.HasRelationship.Value)
+        {
+            return RedirectToRoute(RouteNames.OneAccountNoRelationship, new { ukprn });
+        }
+
         return RedirectToRoute(RouteNames.AddEmployerSearchByEmail, new { ukprn });
     }
 
@@ -77,6 +84,28 @@ public class SearchByEmailController(IOuterApiClient _outerApiClient, ISessionSe
             );
 
         return View(MultipleAccountsShutterPathViewPath, shutterViewModel);
+    }
+
+    [HttpGet]
+    [Route("accountFound", Name = RouteNames.OneAccountNoRelationship)]
+    public IActionResult OneAccountNoRelationshipFound([FromRoute] int ukprn)
+    {
+        var sessionModel = _sessionService.Get<AddEmployerSessionModel>();
+        if (string.IsNullOrEmpty(sessionModel?.Email))
+        {
+            return RedirectToRoute(RouteNames.AddEmployerStart, new { ukprn });
+        }
+
+        var viewModel = new OneAccountNoRelationshipViewModel
+        {
+            CancelLink = Url.RouteUrl(RouteNames.AddEmployerStart, new { ukprn })!,
+            BackLink = Url.RouteUrl(RouteNames.AddEmployerSearchByEmail, new { ukprn })!,
+            ContinueLink = Url.RouteUrl(RouteNames.OneAccountNoRelationship, new { ukprn })!,
+            Ukprn = ukprn,
+            Email = sessionModel.Email
+        };
+
+        return View(OneAccountNoRelationshipViewPath, viewModel);
     }
 
     private SearchByEmailViewModel GetViewModel(int ukprn)
---
+++ src/SFA.DAS.Provider.PR.Web/Models/AddEmployer/OneAccountNoRelationshipViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.Provider.PR.Web.Models.AddEmployer;
+
+public class OneAccountNoRelationshipViewModel
+{
+    public required string CancelLink { get; init; }
+    public required string BackLink { get; init; }
+
+    public required string ContinueLink { get; init; }
+    public required long Ukprn { get; init; }
+    public required string? Email { get; init; }
+}
