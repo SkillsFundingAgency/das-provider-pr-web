daf60f803b8425daa09b387f47afa82e4f553ce4
Deven Shah <devenshah1974@gmail.com>
2024-08-15 14:41:50 +0100

    CSP-1478 Calcuates page links in pagination view model

---
+++ src/SFA.DAS.Provider.PR.Web.UnitTests/Models/PaginationViewModelTests.cs
@@ -0,0 +1,81 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using SFA.DAS.Provider.PR.Web.Models;
+
+namespace SFA.DAS.Provider.PR_Web.UnitTests.Models;
+
+public class PaginationViewModelTests
+{
+    private const int PageSize = 10;
+    [Test]
+    [InlineAutoData(1, 0, 10, "When no records")]
+    [InlineAutoData(3, 10, 10, "When page number is greater than total pages")]
+    public void ReturnsEmptyModel(int currentPage, int totalCount, int pageSize, string testMessage)
+    {
+        var sut = new PaginationViewModel(currentPage, totalCount, pageSize);
+        sut.Pages.Count.Should().Be(0, testMessage);
+    }
+
+    [Test]
+    [InlineAutoData(1, 10, false, "When one page")]
+    [InlineAutoData(6, 60, false, "When total pages are less than equal to 6")]
+    [InlineAutoData(3, 100, false, "When current page is equal to 3")]
+    [InlineAutoData(1, 0, false, "When no records")]
+    [InlineAutoData(4, 70, true, "When two pages previous to current page is greater than 1")]
+    public void AddsPreviousLink(int currentPage, int totalCount, bool expected, string testMessage)
+    {
+        var actual = new PaginationViewModel(currentPage, totalCount, PageSize);
+
+        if (expected)
+        {
+            actual.Pages[0].Title.Should().Be(PaginationViewModel.PreviousPageTitle);
+        }
+        else
+        {
+            actual.Pages.Find(p => p.Title == PaginationViewModel.PreviousPageTitle).Should().BeNull();
+        }
+    }
+
+    [Test]
+    [InlineAutoData(1, 0, false, "When no records")]
+    [InlineAutoData(1, 10, false, "When one page")]
+    [InlineAutoData(7, 100, false, "When on fourth to last page")]
+    [InlineAutoData(8, 100, false, "When on third to last page")]
+    [InlineAutoData(9, 100, false, "When on second to last page")]
+    [InlineAutoData(10, 100, false, "When on last page")]
+    [InlineAutoData(6, 100, true, "When the number of total pages is beyond display range")]
+    public void AddsNextLink(int currentPage, int totalCount, bool expected, string testMessage)
+    {
+        var actual = new PaginationViewModel(currentPage, totalCount, PageSize);
+
+        if (expected)
+        {
+            actual.Pages[actual.Pages.Count - 1].Title.Should().Be(PaginationViewModel.NextPageTitle);
+        }
+        else
+        {
+            actual.Pages.Find(p => p.Title == PaginationViewModel.NextPageTitle).Should().BeNull();
+        }
+    }
+
+    [TestCase(1, 1, 1, "1", "1", "When there is only 1 page")]
+    [TestCase(1, 60, 6, "1", "6", "When current page is 1 and there are 6 pages")]
+    [TestCase(6, 60, 6, "1", "6", "When total pages are less than maximum page links")]
+    [TestCase(1, 70, 7, "1", PaginationViewModel.NextPageTitle, "When current page is 1 and total pages are greater than maximum page links")]
+    [TestCase(3, 70, 7, "1", PaginationViewModel.NextPageTitle, "When current page and first page are both in range and there are more pages")]
+    [TestCase(4, 70, 7, PaginationViewModel.PreviousPageTitle, "7", "When current page and first page are both in range and there are previous pages")]
+    [TestCase(7, 70, 7, PaginationViewModel.PreviousPageTitle, "7", "When current page is the last page in range and there are previous pages")]
+    [TestCase(4, 100, 8, PaginationViewModel.PreviousPageTitle, PaginationViewModel.NextPageTitle, "When there are pages before and after the range")]
+    public void CalculatesCorrectStartPage(int currentPage, int totalCount, int expectedPageLinksCount, string expectedStartPage, string expectedEndPage, string testMessage)
+    {
+        var model = new PaginationViewModel(currentPage, totalCount, PageSize);
+
+        using (new AssertionScope(testMessage))
+        {
+            model.Pages.Count.Should().Be(expectedPageLinksCount, $"Expected page count: {expectedPageLinksCount}");
+            model.Pages[0].Title.Should().Be(expectedStartPage, $"Expected start page: {expectedStartPage}");
+            model.Pages[model.Pages.Count - 1].Title.Should().Be(expectedEndPage, $"Expected end page: {expectedEndPage}");
+        }
+    }
+}
---
+++ src/SFA.DAS.Provider.PR.Web/Models/PaginationViewModel.cs
@@ -0,0 +1,81 @@
+﻿namespace SFA.DAS.Provider.PR.Web.Models;
+
+public class PaginationViewModel
+{
+    public const string PreviousPageTitle = "« Previous";
+    public const string NextPageTitle = "Next »";
+
+    public List<PageLink> Pages { get; } = [];
+
+    public PaginationViewModel(int currentPage, int totalCount, int pageSize)
+    {
+        if (totalCount == 0) return;
+        var totalPages = Convert.ToInt32(Math.Ceiling((double)totalCount / pageSize));
+        if (currentPage > totalPages) return;
+        var (startPage, endPage) = GetPageRange(currentPage, totalCount, pageSize);
+        AddPreviousLinkIfApplicable(totalPages, startPage);
+        AddPageLinks(currentPage, startPage, endPage);
+        AddNextLinkIfApplicable(totalPages, endPage);
+    }
+
+    private void AddNextLinkIfApplicable(int totalPages, int endPage)
+    {
+        if (endPage < totalPages) Pages.Add(new(NextPageTitle, GetPageLink(endPage + 1)));
+    }
+
+    private void AddPageLinks(int currentPage, int startPage, int endPage)
+    {
+        for (int pageNumber = startPage; pageNumber <= endPage; pageNumber++)
+        {
+            string? pageUrl = pageNumber == currentPage ? null : GetPageLink(pageNumber);
+            Pages.Add(new(pageNumber.ToString(), pageUrl));
+        }
+    }
+
+    private void AddPreviousLinkIfApplicable(int totalPages, int startPage)
+    {
+        if (startPage > 1 && totalPages > 6) Pages.Add(new(PreviousPageTitle, GetPageLink(startPage - 1)));
+    }
+
+    private static string? GetPageLink(int pageNumber)
+    {
+        return pageNumber.ToString();
+    }
+
+    static (int startPage, int endPage) GetPageRange(int currentPage, int totalRecords, int pageSize)
+    {
+        int totalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
+
+        int startPage = Math.Max(1, currentPage - 2);
+        int endPage = Math.Min(totalPages, currentPage + 3);
+
+        if (endPage - startPage < 5)
+        {
+            if (startPage == 1)
+            {
+                endPage = Math.Min(totalPages, startPage + 5);
+            }
+            else if (endPage == totalPages)
+            {
+                startPage = Math.Max(1, endPage - 5);
+            }
+        }
+
+        if ((totalPages > 6) && (endPage - startPage < 5))
+        {
+            if (startPage == 1)
+            {
+                endPage = startPage + 5;
+            }
+            else if (endPage == totalPages)
+            {
+                startPage = endPage - 5;
+            }
+        }
+
+
+        return (startPage, endPage);
+    }
+}
+
+public record PageLink(string Title, string? Url);
