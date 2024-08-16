8690771497cbce528869247bef8f54c236530409
Deven Shah <devenshah1974@gmail.com>
2024-08-16 10:11:12 +0100

    CSP-1478 Fixed route issue in home controller

--- src/SFA.DAS.Provider.PR.Web.UnitTests/Controllers/HomeControllerTests.cs
+++ src/SFA.DAS.Provider.PR.Web.UnitTests/Controllers/HomeControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Provider.PR.Web.Controllers;
 using SFA.DAS.Provider.PR.Web.Infrastructure;
@@ -9,7 +10,7 @@ namespace SFA.DAS.Provider.PR_Web.UnitTests.Controllers;
 public class HomeControllerTests
 {
     [Test]
-    public void Index_RedirectsToIndexWithUkprn()
+    public void Index_RedirectsToIndex()
     {
         HomeController sut = new();
         sut.AddDefaultContext();
@@ -20,5 +21,19 @@ public class HomeControllerTests
         actual.As<RedirectToRouteResult>().RouteName.Should().Be(RouteNames.Employers);
         actual.As<RedirectToRouteResult>().RouteValues.Should().ContainKey("ukprn");
         actual.As<RedirectToRouteResult>().RouteValues!["ukprn"].Should().Be(TestConstants.DefaultUkprn.ToString());
+    }
+
+    [Test, AutoData]
+    public void Index_RedirectsToIndexWithUkprn(int ukprn)
+    {
+        HomeController sut = new();
+        sut.AddDefaultContext();
+
+        var actual = sut.Index(ukprn);
+
+        actual.Should().BeOfType<RedirectToRouteResult>();
+        actual.As<RedirectToRouteResult>().RouteName.Should().Be(RouteNames.Employers);
+        actual.As<RedirectToRouteResult>().RouteValues.Should().ContainKey("ukprn");
+        actual.As<RedirectToRouteResult>().RouteValues!["ukprn"].Should().Be(ukprn);
     }
 }
--- src/SFA.DAS.Provider.PR.Web/Controllers/HomeController.cs
+++ src/SFA.DAS.Provider.PR.Web/Controllers/HomeController.cs
@@ -7,15 +7,21 @@ using SFA.DAS.Provider.PR.Web.Infrastructure;
 namespace SFA.DAS.Provider.PR.Web.Controllers;
 
 [Authorize(Policy = nameof(PolicyNames.HasProviderAccount))]
+[Route("")]
 public class HomeController() : Controller
 {
     [HttpGet]
-    [Route("")]
-    [Route("/{ukprn}", Name = RouteNames.Home)]
     public IActionResult Index()
     {
         var ukprn = User.GetUkprn();
 
+        return RedirectToRoute(RouteNames.Employers, new { ukprn });
+    }
+
+    [HttpGet]
+    [Route("/{ukprn}", Name = RouteNames.Home)]
+    public IActionResult Index(int ukprn)
+    {
         return RedirectToRoute(RouteNames.Employers, new { ukprn });
     }
 }
