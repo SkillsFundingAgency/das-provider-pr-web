9d5fa77397e7d1c01abcb04076684462401c8905
Deven Shah <devenshah1974@gmail.com>
2024-10-17 11:02:27 +0100

    CSP-1703 Added data protection for DfE signin (#37)

---
+++ src/SFA.DAS.Provider.PR.Web/AppStart/AddDataProtectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.DataProtection;
+using SFA.DAS.Provider.PR.Web.Infrastructure.Configuration;
+using StackExchange.Redis;
+
+namespace SFA.DAS.Provider.PR.Web.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class AddDataProtectionExtension
+{
+    public static IServiceCollection AddDataProtection(this IServiceCollection services, IConfiguration configuration)
+    {
+        var config = configuration.GetSection(nameof(ApplicationSettings))
+            .Get<ApplicationSettings>();
+
+        if (config != null
+            && !string.IsNullOrEmpty(config.DataProtectionKeysDatabase)
+            && !string.IsNullOrEmpty(config.RedisConnectionString))
+        {
+            var redisConnectionString = config.RedisConnectionString;
+            var dataProtectionKeysDatabase = config.DataProtectionKeysDatabase;
+
+            var redis = ConnectionMultiplexer
+                .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
+
+            services.AddDataProtection()
+                .SetApplicationName("das-provider-web")
+                .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
+        }
+
+        return services;
+    }
+}
