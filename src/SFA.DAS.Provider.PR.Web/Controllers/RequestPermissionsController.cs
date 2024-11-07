014a74077ee470ab3183037496794cb79ef4736e
Peter Williams <peter.david.williams.1991@gmail.com>
2024-11-07 12:16:01 +0000

    CSP-1360 - Added permissions request cshtml page with temp controller action

---
+++ src/SFA.DAS.Provider.PR.Web/Controllers/RequestPermissionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Provider.PR.Web.Infrastructure;
+using SFA.DAS.Provider.PR.Web.Models;
+
+namespace SFA.DAS.Provider.PR.Web.Controllers;
+public class RequestPermissionsController : Controller
+{
+    [HttpGet]
+    [Route("{ukprn:int}/employers/{accountlegalentityid}/permissionsRequested", Name = RouteNames.RequestPermissionsConfirmation)]
+    public async Task<IActionResult> PermissionsRequested([FromRoute] int ukprn, CancellationToken cancellationToken)
+    {
+        //Get session model for employer name (plus whatever is needed for the api call)
+        //API call goes here
+        var employerName = "TEMP NAME";
+
+        var viewModel = new RequestPermissionsConfirmationViewModel
+        {
+            Ukprn = ukprn,
+            AccountLegalEntityName = employerName,
+            EmployersLink = Url.RouteUrl(RouteNames.Employers, new { ukprn })!
+        };
+
+        return View(viewModel);
+    }
+}
---
+++ src/SFA.DAS.Provider.PR.Web/Models/RequestPermissionsConfirmationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Provider.PR.Web.Models;
+
+public class RequestPermissionsConfirmationViewModel
+{
+    public int Ukprn { get; set; }
+    public string AccountLegalEntityName { get; set; }
+    public string EmployersLink { get; set; } = null!;
+}
