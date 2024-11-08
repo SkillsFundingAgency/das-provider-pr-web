478943ffce8ac4332e6468c30c45fab8fb84138c
shaun-downey-education <shaun.downey@education.gov.uk>
2024-11-08 16:14:06 +0000

    CSP-1514: RequestPermissionsSubmitModelValidator - Added unit test coverage.

---
+++ src/SFA.DAS.Provider.PR.Web.UnitTests/Validators/RequestPermissionsSubmitModelValidatorTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation.TestHelper;
+using SFA.DAS.Provider.PR.Web.Constants;
+using SFA.DAS.Provider.PR.Web.Models;
+using SFA.DAS.Provider.PR.Web.Validators;
+
+namespace SFA.DAS.Provider.PR_Web.UnitTests.Validators;
+
+public sealed class RequestPermissionsSubmitModelValidatorTests
+{
+    [Test]
+    public void RequestPermissionsSubmitModelValidator_MatchingPermissions_ReturnInvalid()
+    {
+        var model = new RequestPermissionsSubmitModel()
+        {
+            PermissionToAddCohorts = SetPermissions.AddRecords.Yes,
+            PermissionToRecruit = SetPermissions.RecruitApprentices.Yes,
+            ExistingPermissionToAddCohorts = SetPermissions.AddRecords.Yes,
+            ExistingPermissionToRecruit = SetPermissions.RecruitApprentices.Yes
+        };
+        var sut = new RequestPermissionsSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(a => a.PermissionToAddCohorts)
+            .WithErrorMessage(RequestPermissionsSubmitModelValidator.MatchesExistingPermissionErrorMessage);
+    }
+
+    [Test]
+    public void RequestPermissionsSubmitModelValidator_EmptySelections_ReturnInvalid()
+    {
+        var model = new RequestPermissionsSubmitModel()
+        {
+            PermissionToAddCohorts = string.Empty,
+            PermissionToRecruit = string.Empty,
+            ExistingPermissionToAddCohorts = SetPermissions.AddRecords.Yes,
+            ExistingPermissionToRecruit = SetPermissions.RecruitApprentices.Yes
+        };
+        var sut = new RequestPermissionsSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(a => a.PermissionToRecruit)
+            .WithErrorMessage(RequestPermissionsSubmitModelValidator.RequestPermissionsRecruitApprenticesErrorMessage);
+
+        result.ShouldHaveValidationErrorFor(a => a.PermissionToAddCohorts)
+            .WithErrorMessage(RequestPermissionsSubmitModelValidator.RequestPermissionsAddRecordsErrorMessage);
+    }
+
+    [Test]
+    public void RequestPermissionsSubmitModelValidator_Valid()
+    {
+        var model = new RequestPermissionsSubmitModel()
+        {
+            PermissionToAddCohorts = SetPermissions.AddRecords.Yes,
+            PermissionToRecruit = SetPermissions.RecruitApprentices.Yes,
+            ExistingPermissionToAddCohorts = SetPermissions.AddRecords.No,
+            ExistingPermissionToRecruit = SetPermissions.RecruitApprentices.No
+        };
+        var sut = new RequestPermissionsSubmitModelValidator();
+        var result = sut.TestValidate(model);
+
+        result.ShouldNotHaveValidationErrorFor(a => a.PermissionToRecruit);
+        result.ShouldNotHaveValidationErrorFor(a => a.PermissionToAddCohorts);
+    }
+}
