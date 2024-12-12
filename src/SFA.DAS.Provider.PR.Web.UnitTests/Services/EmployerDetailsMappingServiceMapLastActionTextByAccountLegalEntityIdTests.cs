d280eb15f87f7cf4760288e9be8a36bb23b7c6e6
Peter Williams <peter.david.williams.1991@gmail.com>
2024-12-12 13:04:57 +0000

    CSP-1751 - Updated coverage to include date check for employer led action

--- src/SFA.DAS.Provider.PR.Web.UnitTests/Services/EmployerDetailsMappingServiceMapLastActionTextByAccountLegalEntityIdTests.cs
+++ src/SFA.DAS.Provider.PR.Web.UnitTests/Services/EmployerDetailsMappingServiceMapLastActionTextByAccountLegalEntityIdTests.cs
@@ -5,35 +5,49 @@ using SFA.DAS.Provider.PR.Web.Services;
 namespace SFA.DAS.Provider.PR_Web.UnitTests.Services;
 public class EmployerDetailsMappingServiceMapLastActionTextByAccountLegalEntityIdTests
 {
+    public const string TodayDateTime = "01/01/2020";
+    public const string YesterdayDateTime = "31/12/2019";
+
     [Test]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypeAddAccount, PermissionAction.AccountCreated,
-        RequestStatus.Accepted, EmployerDetailsMappingService.CreateOrAddAccountRequestAcceptedText)]
+        RequestStatus.Accepted, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.CreateOrAddAccountRequestAcceptedText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypeAddAccount, PermissionAction.AccountAdded,
-        RequestStatus.Accepted, EmployerDetailsMappingService.PermissionSetText)]
+        RequestStatus.Accepted, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.PermissionSetText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypePermission, PermissionAction.PermissionUpdated,
-        RequestStatus.Accepted, EmployerDetailsMappingService.PermissionSetText)]
+        RequestStatus.Sent, TodayDateTime, YesterdayDateTime,
+        EmployerDetailsMappingService.PermissionSetText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypePermission, PermissionAction.AccountCreated,
-        RequestStatus.Accepted, EmployerDetailsMappingService.PermissionSetText)]
+        RequestStatus.Accepted, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.PermissionSetText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypePermission, PermissionAction.AccountCreated,
-        RequestStatus.Declined, EmployerDetailsMappingService.UpdatePermissionRequestDeclinedText)]
+        RequestStatus.Declined, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.UpdatePermissionRequestDeclinedText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypePermission, PermissionAction.AccountCreated,
-        RequestStatus.Expired, EmployerDetailsMappingService.UpdatePermissionRequestExpiredText)]
+        RequestStatus.Expired, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.UpdatePermissionRequestExpiredText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypeAddAccount, PermissionAction.PermissionUpdated,
-        RequestStatus.Accepted, EmployerDetailsMappingService.PermissionSetText)]
+        RequestStatus.Accepted, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.PermissionSetText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypeAddAccount, PermissionAction.RecruitRelationship,
-        RequestStatus.Accepted, EmployerDetailsMappingService.RelationshipByRecruitText)]
+        RequestStatus.Accepted, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.RelationshipByRecruitText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypeAddAccount, PermissionAction.ApprovalsRelationship,
-        RequestStatus.Accepted, EmployerDetailsMappingService.RelationshipByApprovalText)]
+        RequestStatus.Accepted, TodayDateTime, TodayDateTime,
+        EmployerDetailsMappingService.RelationshipByApprovalText)]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypePermission, PermissionAction.AccountCreated,
-        RequestStatus.New, "")]
+        RequestStatus.New, TodayDateTime, TodayDateTime, "")]
     [InlineAutoData(EmployerDetailsMappingService.RequestTypeAddAccount, PermissionAction.PermissionUpdated,
-        RequestStatus.New, "")]
+        RequestStatus.New, TodayDateTime, TodayDateTime, "")]
     public void MapTextByAccountLegalEntityId_LastActionTextIsSet(string requestType, PermissionAction lastAction,
-        RequestStatus lastRequestStatus, string expected, GetProviderRelationshipResponse response)
+        RequestStatus lastRequestStatus, string lastActionTime, string lastRequestTime, string expected, GetProviderRelationshipResponse response)
     {
         response.LastRequestType = requestType;
         response.LastAction = lastAction;
         response.LastRequestStatus = lastRequestStatus;
+        response.LastActionTime = DateTime.Parse(lastActionTime);
+        response.LastRequestTime = DateTime.Parse(lastRequestTime);
 
         var result = EmployerDetailsMappingService.MapLastActionText(response);
 
